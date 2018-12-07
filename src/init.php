c78afb294dc4589944e4c65b986de1bd6c9d79cc
s56bouya <s56bouya@gmail.com>
2018-12-07 11:00:35 +0900

    Add : Support WP 5.0

--- src/init.php
+++ src/init.php
@@ -13,28 +13,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-/**
- * Enqueue Gutenberg block assets for both frontend + backend.
- *
- * `wp-blocks`: includes block type registration and related functions.
- *
- * @since 1.0.0
- */
-function nishiki_blocks_cgb_block_assets() {
-	// Styles.
-	wp_enqueue_style(
-		'nishiki_blocks-cgb-style-css', // Handle.
-		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ), // Block style CSS.
-		array( 'wp-blocks' ) // Dependency to include the CSS after it.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' ) // Version: filemtime — Gets file modification time.
-	);
-} // End function nishiki_blocks_cgb_block_assets().
-
-// Hook: Frontend assets.
-add_action( 'enqueue_block_assets', 'nishiki_blocks_cgb_block_assets' );
 
 /**
- * Enqueue Gutenberg block assets for backend editor.
+ * Enqueue Gutenberg block assets for frontend and backend editor.
  *
  * `wp-blocks`: includes block type registration and related functions.
  * `wp-element`: includes the WordPress Element abstraction for describing the structure of your blocks.
@@ -42,43 +23,54 @@ add_action( 'enqueue_block_assets', 'nishiki_blocks_cgb_block_assets' );
  *
  * @since 1.0.0
  */
-function nishiki_blocks_cgb_editor_assets() {
+function nishiki_blocks_editor_assets() {
 	// Scripts.
-	wp_enqueue_script(
-		'nishiki_blocks-cgb-block-js', // Handle.
-		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ), // Block.build.js: We register the block here. Built with Webpack.
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components', 'wp-editor' ), // Dependencies, defined above.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.build.js' ), // Version: filemtime — Gets file modification time.
+	wp_register_script(
+		'nishiki-blocks-js', // Handle.
+		plugins_url( '/dist/blocks.build.js', dirname( __FILE__ ) ),
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components', 'wp-editor' ),
 		true // Enqueue the script in the footer.
 	);
 
 	// Styles.
-	wp_enqueue_style(
-		'nishiki_blocks-cgb-block-editor-css', // Handle.
-		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ), // Block editor CSS.
-		array( 'wp-edit-blocks' ) // Dependency to include the CSS after it.
-		// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: filemtime — Gets file modification time.
+	wp_register_style(
+		'nishiki-blocks-editor-css', // Handle.
+		plugins_url( 'dist/blocks.editor.build.css', dirname( __FILE__ ) ),
+		array()
+//		array( 'wp-edit-blocks' )
 	);
-} // End function nishiki_blocks_cgb_editor_assets().
 
-// Hook: Editor assets.
-add_action( 'enqueue_block_editor_assets', 'nishiki_blocks_cgb_editor_assets' );
-
-// Add Block Category,
-/*
-add_filter( 'block_categories', function( $categories, $post ) {
-	return array_merge(
-		$categories,
-		array(
-			array(
-				'slug' => 'nishiki-blocks',
-				'title' => __( 'Nishiki Blocks（Beta）', 'nishiki' ),
-			),
-		)
+	// Front End Styles.
+	wp_register_style(
+		'nishiki-blocks-style-css',
+		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ),
+		array()
+	// filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.style.build.css' )
 	);
-}, 10, 2 );
-*/
 
+	if( function_exists( 'register_block_type' ) ){
+		register_block_type( 'nishiki/balloon', array(
+			'editor_script'     => 'nishiki-blocks-js',
+			'editor_style'      => 'nishiki-blocks-editor-css',
+			'style'             => 'nishiki-blocks-style-css',
+		) );
+
+		register_block_type( 'nishiki/frame', array(
+			'editor_script'     => 'nishiki-blocks-js',
+			'editor_style'      => 'nishiki-blocks-editor-css',
+		) );
+
+		register_block_type( 'nishiki/section', array(
+			'editor_script'     => 'nishiki-blocks-js',
+			'editor_style'      => 'nishiki-blocks-editor-css',
+		) );
+	}
+}
+
+// Hook: Frontend and Editor assets.
+add_action( 'init', 'nishiki_blocks_editor_assets' );
+
+// Add Block Category.
 function nishiki_blocks_categories( $categories, $post ) {
 	return array_merge(
 		$categories,
